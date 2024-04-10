78c33ee1819233315b164306dd4be4e4d609e259
Test <ultroninfinity01@gmail.com>
2024-04-10 13:33:36 +0530

    Roll-No. 53 Branch - AI

---
+++ 2nd_sem/Pull_Requests/SaurabhKumar_AI_53/Problem1.cpp
@@ -0,0 +1,25 @@
+/*
+  Name - Saurabh Kumar
+  Email - ultroninfinity01@gmail.com
+  Branch - AI
+  Roll no. - 53
+*/
+
+#include<iostream>
+using namespace std;
+
+int main () {
+
+  int n;
+  cin >> n;
+
+  while (true) {
+    cout << n << " ";
+    if (n == 1) { break; };
+    if (n % 2 == 0) { n = n / 2 ; }
+    else {n = n *3 + 1 ;}
+  }
+  cout << "\n";
+
+  return 0;
+}
