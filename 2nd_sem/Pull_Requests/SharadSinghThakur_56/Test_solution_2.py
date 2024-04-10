290a331f7f894d8ff2c0477895c969aef5ab7db8
FreakyOne700 <155244139+FreakyOne700@users.noreply.github.com>
2024-04-10 22:38:35 +0530

    Update Test_solution_2.py

--- 2nd_sem/Pull_Requests/SharadSinghThakur_56/Test_solution_2.py
+++ 2nd_sem/Pull_Requests/SharadSinghThakur_56/Test_solution_2.py
@@ -3,13 +3,12 @@ def solution(n):
     sum = 0
     for i in range(1,n):
         if i%3==0 and i%5==0:
-            print(i)
-            
+            sum += i
         elif i % 5 == 0:
-            sum +=i
+            sum += i
             print(i)
         elif i%3==0 :
-            sum+=i
+            sum += i
         i+=1
     return sum
 
