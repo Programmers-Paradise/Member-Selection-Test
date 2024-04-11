d9829abc13eee0e5316753ad89e5e62463ae7978
Lakshya Sori <166229061+Imaginationsoulofdreams@users.noreply.github.com>
2024-04-11 15:07:14 +0530

    Update problem-1.c

--- 2nd_sem/Pull_Requests/Lakshya_Sori_DS/problem-1.c
+++ 2nd_sem/Pull_Requests/Lakshya_Sori_DS/problem-1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void algorithm(int i);
+void function(int i);
 int main() {
     int i;
     printf("Enter  integer: ");
@@ -10,10 +10,10 @@ int main() {
         return 1;
     }
     printf("Sequence for i = %d:\n", i);
-    Algorithm(i);
+    function(i);
     return 0;
 }
-void algorithm(int i) {
+void function(int i) {
     while (i != 1) {
         printf("%d -> ", i);
         if (i % 2 == 0) {
@@ -24,5 +24,5 @@ void algorithm(int i) {
             i = 3 * i + 1;
         }
     }
-    printf("1\n"); 
+    printf("thank you\n"); 
 }
