f755a12df8f0b79869ba4b77eb5e2f4ed0444caa
Ashirwad - San <ashirwadrai5879@outlook.com>
2024-04-11 13:51:42 +0530

    solution_for_test

---
+++ 4th_sem/Pull_Requests/Ashirwad_300012722046_AI/46_AI_solution1.py
@@ -0,0 +1,27 @@
+# Ashirwad Kumar  Email --> ashirwadrai5879@outlook.com
+
+def minm_rooms(customers):
+    custom_info = [(arrival, departure, i) for i, (arrival, departure) in enumerate(customers)]
+    custom_info = sorted(custom_info)
+    rooms = []
+    room_allocation = [0] * len(customers)
+
+    for arrival, departure, i in custom_info:
+        assigned = False
+        for room in rooms:
+            if room[-1][1] < arrival:
+                room.append((arrival, departure))
+                room_allocation[i] = rooms.index(room) + 1
+                assigned = True
+                break
+        if not assigned:
+            rooms.append([(arrival, departure)])
+            room_allocation[i] = len(rooms)
+    print(len(rooms))
+    print(*room_allocation)
+
+
+
+customers = [(6, 6), (5, 5), (6, 6), (5, 10)]
+minm_rooms(customers)
+
