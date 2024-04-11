4b95b3716affe7a7b8cf4e54365b0b60c63ffbd2
Rupali Chandrakar <148358159+rupali12chandrakar@users.noreply.github.com>
2024-04-11 13:53:39 +0530

    Soln_2.py

---
+++ 2nd_sem/Pull_Requests/Soln_2.py
@@ -0,0 +1,9 @@
+total_sum = 0 
+
+  for e in range(1, 10): #Iterate over the range from 1 to 9         
+  
+  if (e % 3 == 0 or e % 5 == 0):  #Check if the current number e is a multiple of 3 or 5 using the condition                 
+    
+  total_sum = total_sum + e        
+    
+   print ( "Total sum is :" , total_sum )
