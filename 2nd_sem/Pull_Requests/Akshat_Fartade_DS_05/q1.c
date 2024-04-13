72993781e91b0337d0febe4046a73e69523e9328
AkshatFartade1 <166852853+AkshatFartade1@users.noreply.github.com>
2024-04-13 15:20:47 +0530

    added sol.

---
+++ 2nd_sem/Pull_Requests/Akshat_Fartade_DS_05/q1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n=3;
+    for(;n!=1;)
+    {
+    if(n%2==0){
+        n/=2;
+    }
+    else 
+    {
+       n=n*3+1;
+    }}
+    printf("The value of n is %d",n);
+    return 0;
+    
+}
